b7f508d5a58b8d7427be47a6c2c71e63bb697819
Evan Basalik <evanba@microsoft.com>
2019-08-20 20:22:05 -0400

    Works up until trying to kick off a trace.

--- Netwatcher-VMSS/netwatcher-vmss.ps1
+++ Netwatcher-VMSS/netwatcher-vmss.ps1
@@ -5,24 +5,85 @@ Azure services parameters
 $rgName = "<resource group name>"
 $location = "<region hosting the VMSS>"
 $VMSSName = "<Underlying VMSS name>" 
-$storageAccountName = "<storage account to put the packet captures>"
-$VMSS = Get-AzureRmVmss -VMScaleSetName $VMSSName -ResourceGroupName $rgName
+$storageAccountName = $VMSSName + "<storage account to put the packet captures>"
+$containerName = "networktraces"
+$networkWatcherName = "<Network Watcher name>"
+
+$rgName = "rgVMSSNW"
+$location ="East US"
+$VMSSName = "vmssevanb"
+$storageAccountName = $VMSSName + "vmsstest"
+$containerName = "networktraces"
+$networkWatcherName = "nw"
+
+#Log in if necessary
+if (!Get-AzContext)
+{
+    Connect-AzAccount
+}
+
+#Validate that the Storage account exists and create if not
+Write-Host "Creating/validating Storage Account $storageAccountName"
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $rgName | Where-Object {$_.StorageAccountName -eq $storageAccountName}
+if (($storageAccount).count -eq 0)
+{
+    #Account didn't exist, create 
+    Write-Host "Creating Storage Account $VMSSName$storageAccountName"
+    $storageAccount = New-AzStorageAccount -StorageAccountName $storageAccountName -Location $Location -ResourceGroupName $rgName -SkuName Standard_GRS
+}
+else
+{
+    Write-Host "Storage Account exists"
+}
+
+#Validate that the container exists
+Write-Host "Creating/validating container $containerName"
+if ((Get-AzStorageContainer -Context $storageAccount.Context | Where-Object {$_.Name -eq $containerName}).count -eq 0)
+{
+    #Container didn't exist, create
+    Write-Host "Creating container $containerName"
+    New-AzStorageContainer -Name $containerName -Context $storageAccount.Context
+}
+else
+{
+    Write-Host "Container exists"
+}
+
+#Grab a pointer to the VMSS
+Write-Host "Getting a pointer to the VMSS"
+$VMSS = Get-AzVmss -VMScaleSetName $VMSSName -ResourceGroupName $rgName
 
 #Add Network Watcher extension to the VMSS
-Add-AzureRmVmssExtension -VirtualMachineScaleSet $VMSS -Name "networkWatcherAgent" -Publisher "Microsoft.Azure.NetworkWatcher" -Type "NetworkWatcherAgentWindows" -TypeHandlerVersion "1.4" -AutoUpgradeMinorVersion $True
-
-#Get VM from underlying VMSS
-$VM1 = Get-AzureRmVmssVM -ResourceGroupName $rgName -VMScaleSetName "<name of VMSS>" -InstanceId "0"
-$VM2 = Get-AzureRmVmssVM -ResourceGroupName $rgName -VMScaleSetName "<name of VMSS>" -InstanceId "1"
+Write-Host "Adding Network Watcher to VMSS instances"
+$nwExt = Get-AzVMExtensionImage -Location $location -PublisherName Microsoft.Azure.NetworkWatcher -Type NetworkWatcherAgentWindows
+Set-AzVmssExtension -VirtualMachineScaleSet $VMSS -Name $nwExt.Name -Publisher $nwExt.PublisherName -AutoUpgradeMinorVersion $True
 
 #Get Network Watcher Object
-$nw = Get-AzureRmResource | Where-Object { $_.ResourceType -eq "Microsoft.Network/networkWatchers" -and $_.Location -eq $location } 
-$networkWatcher = Get-AzureRmNetworkWatcher -Name $nw.Name -ResourceGroupName $nw.ResourceGroupName 
-$storageAccount = Get-AzureRmStorageAccount -ResourceGroupName $rgName  -Name $storageAccountName
+Write-Host "Getting a pointer to Network Watcher"
+if ((Get-AzNetworkWatcher -ResourceGroupName $rgName -Name $networkWatcherName).count -eq 0)
+{
+    $networkWatcher = New-AzNetworkWatcher -Name $networkWatcherName -ResourceGroupName $rgName -Location $location
+}
+else 
+{
+    $networkWatcher = Get-AzNetworkWatcher -ResourceGroupName $rgName -Name $networkWatcherName
+}
 
 #Filters that we can tune to the solution
-$filter1 = New-AzureRmPacketCaptureFilterConfig -Protocol TCP -RemoteIPAddress "1.1.1.1-255.255.255" -LocalIPAddress "10.0.0.3" -LocalPort "1-65535" -RemotePort "20;80;443"
-$filter2 = New-AzureRmPacketCaptureFilterConfig -Protocol UDP 
+Write-Host "Creating some packet filters"
+$filter1 = New-AzPacketCaptureFilterConfig -Protocol TCP -RemoteIPAddress "1.1.1.1-255.255.255" -LocalIPAddress "10.0.0.3" -LocalPort "1-65535" -RemotePort "20;80;443"
+$filter2 = New-AzPacketCaptureFilterConfig -Protocol UDP 
 
-#Run the packet capture with a unique packet capture name
-$packet = New-AzureRmNetworkWatcherPacketCapture -NetworkWatcher $networkWatcher -TargetVirtualMachineId $vm2.Id -PacketCaptureName "<packet capture name>" -StorageAccountId $storageAccount.id -TimeLimitInSeconds 60 -Filter $filter1, $filter2
+#Loop through and set up Network Watcher on each VM in the VMSS
+Write-Host "Kicking off a packet capture for the entire VMSS - VM by VM"
+for ($i = 0; $i -lt $VMSS.Sku.Capacity; $i++) 
+{
+    #Get VM from underlying VMSS
+    $VM = Get-AzVmssVM -ResourceGroupName $rgName -VMScaleSetName $VMSSName -InstanceId $i
+
+    #Run the packet capture with a unique packet capture name
+    $packetCaptureName = "capture_vm_" + $i  
+    $packetCapture = New-AzNetworkWatcherPacketCapture -NetworkWatcher $networkWatcher -TargetVirtualMachineId $VM.Id -PacketCaptureName $packetCaptureName -StorageAccountId $storageAccount.id -TimeLimitInSeconds 60 -Filter $filter1, $filter2
+    $packetCapture
+}
+
