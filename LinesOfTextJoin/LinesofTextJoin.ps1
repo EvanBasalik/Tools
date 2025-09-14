14d1649b02eadb8bb09db5ef4944446da4c24c75
Evan Basalik <evanba@microsoft.com>
2025-09-14 17:31:31 -0400

    add semi-colon join version

--- LinesOfTextJoin/LinesofTextJoin.ps1
+++ LinesOfTextJoin/LinesofTextJoin.ps1
@@ -1,3 +1,5 @@
 param([string]$inputfile="input.txt", [string]$outputfile="output.txt") 
 $text = Get-Content .\$inputfile -Raw 
 ($text.Split("`n") | %{ $_.Trim() }) -join """,""" >> $outputfile
+
+($text.Split("`n") | ForEach-Object { $_.Trim() }) -join ";" | Set-Content $outputfile
